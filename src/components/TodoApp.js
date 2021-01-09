373725e4a85d8a8ea13d6c47ebdabb72800f52e2
kt-sec <kt-sec>
2021-01-09 14:10:26 +0300

    Null localStorage initialization bug fixed.

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -4,24 +4,23 @@ import Footer from './Todo/Footer';
 import {useState,useEffect} from 'react'
 
 function TodoApp() {
-    const activeLen = JSON.parse(localStorage.getItem("testObject"))
-    .filter(item=>item.done===false).length;
     const getData =  JSON.parse(localStorage.getItem("testObject"));
     const getIndexData = localStorage.getItem("lastIndex");
+    const activeLen = (getData===null?0:getData.filter(item=>item.done===false).length)
     const [list,setList] = useState(getData===null?[]:getData);
     const [listLength,setListLength] = useState(getIndexData===null?[0]:getIndexData);
     const [activeCount,setActiveCount] = useState(activeLen);
     const [isFooterVisible,setIsFooterVisible] = useState(false);
 
-    useEffect(()=>{
-        
+    useEffect(()=>{ 
         const newLen = list.filter(item=>item.done===false).length;
         setActiveCount(newLen);
         
         if(parseInt(list.length)>0){
             setIsFooterVisible(true);
         }else if(parseInt(list.length)===0){
-            const getDataLen =  JSON.parse(localStorage.getItem("testObject")).length;
+            const getData = JSON.parse(localStorage.getItem("testObject"));
+            const getDataLen =  (getData===null?0:getData.length);
             if(getDataLen===0){
                 setIsFooterVisible(false);
             }
